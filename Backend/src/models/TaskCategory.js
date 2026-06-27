0bdf23b43491ef7367074bb3782bdfb4f0d3d93f
Ching Norin <huychy90@gmail.com>
2026-06-27 15:43:44 +0700

    Define TaskCategory model with attributes

--- Backend/src/models/TaskCategory.js
+++ Backend/src/models/TaskCategory.js
@@ -1,1 +1,28 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const TaskCategory = sequelize.define(
+    "TaskCategory",
+    {
+      category_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      category_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      admin_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "task_categories",
+      timestamps: true,
+    }
+  );
+
+  return TaskCategory;
+};
