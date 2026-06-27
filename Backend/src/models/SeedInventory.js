696de60b78847f8b7fe358989e3705eea5dda035
Ching Norin <huychy90@gmail.com>
2026-06-27 15:42:19 +0700

    Define SeedInventory model with fields and settings

--- Backend/src/models/SeedInventory.js
+++ Backend/src/models/SeedInventory.js
@@ -1,1 +1,32 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const SeedInventory = sequelize.define(
+    "SeedInventory",
+    {
+      inventory_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      seed_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "seed_inventories",
+      timestamps: true,
+    }
+  );
+
+  return SeedInventory;
+};
