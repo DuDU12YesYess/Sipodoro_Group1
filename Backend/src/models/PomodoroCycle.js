bef915ae625f2cef74bfc48280a395ff30dd7091
Ching Norin <huychy90@gmail.com>
2026-06-27 15:41:39 +0700

    Define PomodoroCycle model with attributes

--- Backend/src/models/PomodoroCycle.js
+++ Backend/src/models/PomodoroCycle.js
@@ -1,1 +1,39 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const PomodoroCycle = sequelize.define(
+    "PomodoroCycle",
+    {
+      cycle_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      start_time: DataTypes.DATE,
+      end_time: DataTypes.DATE,
+      completed_focus_sessions: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      completed_break: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      streak_earned: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      completed_at: DataTypes.DATE,
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "pomodoro_cycles",
+      timestamps: true,
+    }
+  );
+
+  return PomodoroCycle;
+};
