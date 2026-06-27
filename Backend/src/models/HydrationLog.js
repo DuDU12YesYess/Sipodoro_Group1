695d621f656ff41602c1c6af7d91baf130db43ae
Ching Norin <huychy90@gmail.com>
2026-06-27 15:41:21 +0700

    Define HydrationLog model with Sequelize

--- Backend/src/models/HydrationLog.js
+++ Backend/src/models/HydrationLog.js
@@ -1,1 +1,28 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const HydrationLog = sequelize.define(
+    "HydrationLog",
+    {
+      hydration_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      check_in_time: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "hydration_logs",
+      timestamps: true,
+    }
+  );
+
+  return HydrationLog;
+};
