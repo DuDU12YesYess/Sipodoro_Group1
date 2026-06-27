6c13ccf3f38bb4da5619cdd34264890fc323efc1
Ching Norin <huychy90@gmail.com>
2026-06-27 15:42:57 +0700

    Update StreakRecord.js

--- Backend/src/models/StreakRecord.js
+++ Backend/src/models/StreakRecord.js
@@ -1,1 +1,35 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const StreakRecord = sequelize.define(
+    "StreakRecord",
+    {
+      streak_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      current_streak: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      longest_streak: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      last_completed_cycle: {
+        type: DataTypes.DATE,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        unique: true,
+      },
+    },
+    {
+      tableName: "streak_records",
+      timestamps: true,
+    }
+  );
+
+  return StreakRecord;
+};
