0225863e21ab15eecf8edb8f7475fab19e656aff
Ching Norin <huychy90@gmail.com>
2026-06-27 15:40:47 +0700

    Create Flower model with attributes
    Define Flower model with attributes for growth stage, planting date, blooming date, user ID, and seed ID.

--- Backend/src/models/Flower.js
+++ Backend/src/models/Flower.js
@@ -1,1 +1,37 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Flower = sequelize.define(
+    "Flower",
+    {
+      flower_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      growth_stage: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+      },
+      date_planted: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      date_bloomed: DataTypes.DATE,
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      seed_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "flowers",
+      timestamps: true,
+    }
+  );
+
+  return Flower;
+};
