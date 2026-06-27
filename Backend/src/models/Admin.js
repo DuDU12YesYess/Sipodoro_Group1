ca2439831b7ad1b789a74dce7115684403ca907f
Ching Norin <huychy90@gmail.com>
2026-06-27 15:39:54 +0700

    Define Admin model with Sequelize attributes

--- Backend/src/models/Admin.js
+++ Backend/src/models/Admin.js
@@ -1,1 +1,38 @@
-﻿
+﻿import { DataTypes } from 'sequelize';
+import sequelize from '../database.js';
+
+const Admin = sequelize.define(             // .define() maps a JS object to a table
+  'Admin',                                    // model name (Sequelize uses this as the table name)
+  {
+    admin_id: {
+        type: DataTypes.INTEGER,               
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+    },
+    }
+);
+
+
+export default Admin;
