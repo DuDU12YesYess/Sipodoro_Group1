5e55edbf05035401c56577688e140bcf383a41ce
Ching Norin <huychy90@gmail.com>
2026-06-27 15:42:04 +0700

    Update Seed.js

--- Backend/src/models/Seed.js
+++ Backend/src/models/Seed.js
@@ -1,1 +1,36 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Seed = sequelize.define(
+    "Seed",
+    {
+      seed_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      growth_required: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      cost: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      admin_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "seeds",
+      timestamps: true,
+    }
+  );
+
+  return Seed;
+};
