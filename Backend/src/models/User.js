17e96dd10cf91492dee5c297bd356d836b794312
Ching Norin <huychy90@gmail.com>
2026-06-27 15:44:00 +0700

    Update User.js

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,1 +1,52 @@
-﻿
+﻿const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const User = sequelize.define(
+    "User",
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(150),
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      total_streak: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      total_flowers: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      admin_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: false,
+    }
+  );
+
+  return User;
+};
