19ba92ab3676d6c39d5aec4eb865b4986f72aa46
Ching Norin <huychy90@gmail.com>
2026-06-27 15:53:02 +0700

    Implement task CRUD routes with authentication

--- Backend/src/routes/taskRoutes.js
+++ Backend/src/routes/taskRoutes.js
@@ -1,1 +1,14 @@
-﻿
+﻿const express = require("express");
+const router = express.Router();
+
+const taskController = require("../controllers/taskController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// Task CRUD
+router.get("/", authMiddleware, taskController.getAllTasks);
+router.get("/:id", authMiddleware, taskController.getTaskById);
+router.post("/", authMiddleware, taskController.createTask);
+router.put("/:id", authMiddleware, taskController.updateTask);
+router.delete("/:id", authMiddleware, taskController.deleteTask);
+
+module.exports = router;
